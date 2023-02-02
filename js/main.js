c2ca9a8f643e4c22586412ffdf2c59a8e04ec70c
Guilherme Cardoso <65917790+gdcodedev@users.noreply.github.com>
2023-02-01 23:16:11 -0300

    update

--- js/main.js
+++ js/main.js
@@ -106,22 +106,11 @@ suggestionButtons.forEach((btn) => {
 
 // Resources 
 
-export default {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization' },
-        ],
-      },
-    ];
-  },
-  async redirects() {
-    return [];
-  }
-};
+import helmet from 'helmet';
+const helmet = require("helmet");
+
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
