124f54add4e4291726968281a9ad9c83f4bfe405
Guilherme Cardoso <65917790+gdcodedev@users.noreply.github.com>
2023-02-02 00:24:44 -0300

    main

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-const apiKey = "ff648ee401da4e5d6814e39509c0d913";
+const apiKey = "9a9d22a4335c88ec9a4be4940eb15af9";
 const apiCountryURL = "https://countryflagsapi.com/png/";
 const apiUnsplash = "https://source.unsplash.com/1600x900/?";
 
@@ -67,7 +67,7 @@ const showWeatherData = async (city) => {
   descElement.innerText = data.weather[0].description;
   weatherIconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
   );
   countryElement.setAttribute("src", apiCountryURL + data.sys.country);
   umidityElement.innerText = `${data.main.humidity}%`;
